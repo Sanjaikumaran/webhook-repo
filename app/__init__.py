ac7275f340f40eb20a801f23f9b89b01b4281674
Sanjaikumaran P <sanjaikumaran@makoitlab.com>
2026-03-02 17:08:37 +0530

    Project restructure

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,28 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+from dotenv import load_dotenv
+import os
 
-from app.webhook.routes import webhook
+from .extensions import mongo
+from .webhook.routes import webhook
 
-
-# Creating our flask app
 def create_app():
+    load_dotenv()
+    print("ii")
 
-    app = Flask(__name__)
-    
-    # registering all the blueprints
+    app = Flask(__name__, static_folder="../ui", static_url_path="/ui")
+
+    app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+    mongo.init_app(app)
+
     app.register_blueprint(webhook)
-    
+
+    @app.route("/")
+    def home():
+        return "Webhook server running"
+
+    @app.route("/ui")
+    def ui():
+        return send_from_directory("../ui", "index.html")
+
     return app
--- app/webhook/routes.py
+++ app/webhook/routes.py
@@ -1,7 +1,63 @@
-from flask import Blueprint, json, request
+from flask import Blueprint, request, jsonify
+from datetime import datetime
+import uuid
+from ..extensions import mongo
 
 webhook = Blueprint('Webhook', __name__, url_prefix='/webhook')
 
 @webhook.route('/receiver', methods=["POST"])
 def receiver():
-    return {}, 200
+    payload = request.json
+    event = request.headers.get("X-GitHub-Event")
+
+    doc = {
+        "request_id": str(uuid.uuid4()),
+        "timestamp": datetime.utcnow()
+    }
+
+    if event == "push":
+        doc.update({
+            "author": payload["pusher"]["name"],
+            "action": "push",
+            "from_branch": None,
+            "to_branch": payload["ref"].split("/")[-1]
+        })
+
+    elif event == "pull_request":
+        pr = payload["pull_request"]
+
+        action = "merge" if pr.get("merged") else "pull_request"
+
+        doc.update({
+            "author": pr["user"]["login"],
+            "action": action,
+            "from_branch": pr["head"]["ref"],
+            "to_branch": pr["base"]["ref"]
+        })
+
+    else:
+        return jsonify({"status": "ignored"}), 200
+
+    mongo.db.events.insert_one(doc)
+
+    return jsonify({"status": "stored"}), 200
+
+@webhook.route('/events', methods=["GET"])
+def get_events():
+    docs = list(mongo.db.events.find().sort("timestamp", -1).limit(20))
+
+    events = []
+
+    for d in docs:
+        ts = d["timestamp"].strftime("%d %B %Y - %I:%M %p UTC")
+
+        events.append({
+            "request_id": d["request_id"],
+            "author": d["author"],
+            "action": d["action"],
+            "from_branch": d["from_branch"],
+            "to_branch": d["to_branch"],
+            "timestamp": d["timestamp"].isoformat()
+        })
+
+    return jsonify(events)
